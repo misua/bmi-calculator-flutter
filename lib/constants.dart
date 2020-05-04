2fa0fd5428adc6a7065abb014cf982134b199dc2
charlespogzz@googlemail.com <charlespogzz@googlemail.com>
2020-05-05 06:50:42 +0800

    la

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 80.0;
+const kActivecardColour = Color(0xFF1D1E33);
+const kInactivecardColor = Color(0xFF111328);
+const kBottomcardColour = Color(0xFFEB1555);
+
+const kLabelTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Color(0xFF8D8E98),
+);
+
+const kHugeTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.w900,
+);
